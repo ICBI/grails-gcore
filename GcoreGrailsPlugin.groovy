6a79d95afa2cada5b6c58d97ff5c21eaf0e0eddf
lac223 <lac223@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-03-14 19:16:18 +0000

    gcore plugin
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/branches/gcore@1457 7e94895d-eddb-4489-9ea7-1067dfd99f16

---
+++ GcoreGrailsPlugin.groovy
@@ -0,0 +1,50 @@
+class GcoreGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.3.7 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [recaptcha:'0.5.0',searchable:'0.5.5', taggable:'0.6.1', jquery:'1.3.2.4', jms:'1.1', mail:'0.9',quartz:'0.4.1', seleniumRc:'0.1.1']
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    // TODO Fill in these fields
+    def author = "Your name"
+    def authorEmail = ""
+    def title = "gcore plugin"
+    def description = '''\\
+Brief description of the plugin.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/gcore"
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before 
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+}
