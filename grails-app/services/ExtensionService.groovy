a4ac9d6318d9ff8ee5aea2d765f0a6f6c590c4ce
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-03-16 19:10:43 +0000

    adding methods to extension service
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/branches/gcore@1473 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/services/ExtensionService.groovy
+++ grails-app/services/ExtensionService.groovy
@@ -1,16 +1,29 @@
 import org.codehaus.groovy.grails.commons.GrailsApplication 
 import org.springframework.beans.factory.InitializingBean
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
 
-class ExtensionService implements InitializingBean {
+class ExtensionService implements InitializingBean, ApplicationContextAware {
 	def grailsApplication
 	def extensionMap
+	def analysisServices
+	ApplicationContext applicationContext
 	
 	void afterPropertiesSet() {
 		extensionMap = [:]
+		analysisServices = [:]
 		// Find all extension annotations on the controllers
-		grailsApplication.controllerClasses.each{ 
-			if(it.clazz.getAnnotation(Extension.class)) {
-				extensionMap[it] = it.clazz.getAnnotation(Extension.class)
+		grailsApplication.controllerClasses.each{ controller ->
+			if(controller.clazz.getAnnotation(Extension.class)) {
+				def annotation = controller.clazz.getAnnotation(Extension.class)
+				extensionMap[controller.logicalPropertyName] = annotation
+				
+				// Load up analysis services, looking at requestType on the command and matching it with a service of the same name
+				if(annotation.type() == ExtensionType.ANALYSIS) {
+					def commandClass = Thread.currentThread().contextClassLoader.loadClass(controller.name + 'Command')
+					def analysisService = applicationContext.getBean(controller.logicalPropertyName + 'Service')
+					analysisServices[commandClass.requestType] = analysisService
+				}
 			}
 		}
 	}
@@ -23,11 +36,15 @@ class ExtensionService implements InitializingBean {
 		return buildLinks(ExtensionType.SEARCH)
 	}
 	
+	def buildRequest(userId, cmd) {
+		return analysisServices[cmd.requestType].handle(userId, cmd)
+	}
+	
 	private def buildLinks(type) {
 		def links = [:]
 		extensionMap.each { key, value ->
 			if(value.type() == type) {
-				links[key.logicalPropertyName] = value.menu()
+				links[key] = value.menu()
 			}
 		}
 		return links
