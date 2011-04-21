3db2e094304ac54f1ab498a7dea528ead179e926
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-04-21 17:27:24 +0000

    removing
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gcore-plugins/grails-gcore/trunk@1693 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- gcore/test/integration/AnalysisAnnotationTests.groovy
+++
@@ -1,8 +0,0 @@
-import org.codehaus.groovy.grails.commons.GrailsApplication 
-
-class AnalysisAnnotationTests extends GroovyTestCase {
-	def extensionService
-    void testRuntime() {
-		println extensionService.getAnalysisLinks()
-    }
-}
