d997a42efff72c93460b3a91312f51f75af3f13d
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-04-21 17:10:06 +0000

    releasing plugin
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/branches/gcore@1683 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,3 +1,14 @@
+grails.plugin.repos.distribution.gcore="https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gcore-plugins/"
+grails.plugin.repos.discovery.gcore="https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gcore-plugins/"
+
+/*grails.project.dependency.resolution = {
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsRepo "https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gcore-plugins/"
+		grailsCentral()
+	}
+}*/
 // war file setup
 grails.war.resources = {stagingDir ->
 	delete(file: "$stagingDir/WEB-INF/lib/jbossall-client.jar")
