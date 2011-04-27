aa5bdcbfe91992e973c93543a654ddd37ee6692d
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-04-27 15:39:40 +0000

    releasing new plugin version
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gcore-plugins/grails-gcore/trunk@2059 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- GcoreGrailsPlugin.groovy
+++ GcoreGrailsPlugin.groovy
@@ -13,7 +13,9 @@ class GcoreGrailsPlugin {
 	
 	static LOG = LogFactory.getLog("GcoreGrailsPlugin")
     // the plugin version
-    def version = "0.1"
+    def version = "0.1.1"
+	// groupName
+	def groupId = "edu.georgetown.gcore"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
