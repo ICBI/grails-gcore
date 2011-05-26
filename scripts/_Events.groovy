36b8207a2a2677f9e42b2f4e8a8986d77cda1d9f
kmr75 <kmr75@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-05-26 18:10:27 +0000

    added events.groovy
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gcore-plugins/grails-gcore/trunk@2369 7e94895d-eddb-4489-9ea7-1067dfd99f16

---
+++ scripts/_Events.groovy
@@ -0,0 +1,9 @@
+eventPackagePluginStart = { 
+     // hack to get the sql files into the zip 
+     String dir = "$projectWorkDir/plugin-info/sql" 
+     ant.mkdir dir: dir 
+     ant.copy file: "$basedir/sql/01_study_setup_template.sql", todir: dir 
+     ant.copy file: "$basedir/sql/02_study_schema_template.sql", todir: dir 
+     ant.copy file: "$basedir/sql/03_study_grants_template.sql", todir: dir 
+     ant.copy file: "$basedir/sql/study_cleanup_template.sql", todir: dir 
+}
