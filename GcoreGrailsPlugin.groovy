7c06dfe37f3f24d5fc77e0e6871fe7c4c5be4787
kmr75 <kmr75@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-06-03 16:19:04 +0000

    fix for feed service
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gcore-plugins/grails-gcore/trunk@2423 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- GcoreGrailsPlugin.groovy
+++ GcoreGrailsPlugin.groovy
@@ -12,7 +12,7 @@ class GcoreGrailsPlugin {
 	
 	static LOG = LogFactory.getLog("GcoreGrailsPlugin")
     // the plugin version
-    def version = "0.1.9"
+    def version = "0.2.0"
 	// groupName
 	def groupId = "edu.georgetown.gcore"
     // the version or versions of Grails the plugin is designed for
