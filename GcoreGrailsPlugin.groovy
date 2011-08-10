ba8bc28a1c77399c0d44ac328943ff689e6d1c65
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-08-10 16:52:20 +0000

    fixing bug
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gcore-plugins/grails-gcore/trunk@3021 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- GcoreGrailsPlugin.groovy
+++ GcoreGrailsPlugin.groovy
@@ -12,7 +12,7 @@ class GcoreGrailsPlugin {
 	
 	static LOG = LogFactory.getLog("GcoreGrailsPlugin")
     // the plugin version
-    def version = "0.4.2"
+    def version = "0.4.3"
 	// groupName
 	def groupId = "edu.georgetown.gcore"
     // the version or versions of Grails the plugin is designed for
