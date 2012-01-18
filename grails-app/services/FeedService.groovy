5b8f646c1087dc965766f3225bcd474c86870ea2
kmr75 <kmr75@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2012-01-18 20:44:18 +0000

    modified feed service to account for atom feed
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gcore-plugins/grails-gcore/trunk@3434 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/services/FeedService.groovy
+++ grails-app/services/FeedService.groovy
@@ -9,8 +9,23 @@ class FeedService{
 			feedURL = CH.config.grails.feedURL
 			if(feedURL){
 		    	def xmlFeed = new XmlParser().parse(feedURL);
-				feedMap = [:]
-					 if(xmlFeed.channel && xmlFeed.channel.item){
+				if(xmlFeed.entry){
+					log.debug "read atom feed"
+					if(xmlFeed.entry ){
+						int i = 0
+						xmlFeed.entry.each{
+							if(i < 10){
+								def newsItem = it;
+								//def date = String.format('%tF', newsItem.pubDate.text)
+								feedMap.put(newsItem.title.text(),newsItem.link.@href[0])
+								i++;
+							}
+						}
+
+					 }
+				}else if(xmlFeed.channel){
+					log.debug "read rss feed"
+					if(xmlFeed.channel && xmlFeed.channel.item){
 						int i = 0
 						xmlFeed.channel.item.each{
 							if(i < 10){
@@ -22,6 +37,8 @@ class FeedService{
 						}
 
 					 }
+				}
+					 
 			}
 		}catch (Exception e){
 			log.debug "error parsing URL $feedURL, possible that URL is down - $e"
