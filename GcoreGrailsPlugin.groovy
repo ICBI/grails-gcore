f09f79ac98594e5540c3f7d160dd91c2ea6c7d04
Andrew Shinohara <acs224@georgetown.edu>
2012-04-10 16:05:50 -0400

    adding maven repo info

--- GcoreGrailsPlugin.groovy
+++ GcoreGrailsPlugin.groovy
@@ -13,13 +13,13 @@ class GcoreGrailsPlugin {
 	
 	static LOG = LogFactory.getLog("GcoreGrailsPlugin")
     // the plugin version
-    def version = "0.5.8"
+    def version = "0.6.0"
 	// groupName
 	def groupId = "edu.georgetown.gcore"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [recaptcha:'0.5.2',searchable:'0.5.5', taggable:'0.6.1', jquery:'1.3.2.4', mail:'0.9',quartz:'0.4.1', springSecurityCore:'1.1.2',springSecurityLdap:'1.0.3']
+    def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,3 +6,38 @@ grails.war.resources = {stagingDir ->
 	delete(file: "$stagingDir/WEB-INF/lib/jboss-jca.jar")
 	delete(file: "$stagingDir/WEB-INF/lib/ojdbc14.jar")
 }
+
+grails.project.dependency.resolution = {
+	
+	inherits("global") {
+	        // uncomment to disable ehcache
+	        // excludes 'ehcache'
+	}
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		mavenRepo "https://gdoc-stage.georgetown.edu/artifactory/plugins-release-local/"
+		grailsCentral()
+	}
+	dependencies {
+		compile("org.tmatesoft.svnkit:svnkit:1.3.5") {
+            excludes "jna", "trilead-ssh2", "sqljet"
+			export = false
+        }
+	}
+	
+	plugins {
+		build ':recaptcha:0.5.2'
+		build ':searchable:0.5.5'
+		build ':taggable:0.6.1'
+		build ':jquery:1.3.2.4'
+		build ':mail:0.9'
+		build ':quartz:0.4.1'
+		build ':spring-security-core:1.1.2'
+		build ':spring-security-ldap:1.0.3'
+		build(':svn:1.0.2') {
+			export = false
+		}
+		
+	}
+}
