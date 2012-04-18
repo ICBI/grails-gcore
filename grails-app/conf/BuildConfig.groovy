34e329f1e5e75a1220e9928e83597c2c55da80ee
Andrew Shinohara <acs224@georgetown.edu>
2012-04-18 11:51:45 -0400

    Fixing project dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,13 +29,15 @@ grails.project.dependency.resolution = {
 	plugins {
 		compile ':recaptcha:0.5.2'
 		compile ':searchable:0.5.5'
-		compile ':taggable:0.6.1'
-		compile ':jquery:1.3.2.4'
-		compile ':mail:0.9'
+		compile	':taggable:0.6.1'
+		compile	':jquery:1.3.2.4'
+		compile	':mail:0.9'
 		compile ':quartz:0.4.1'
-		compile ':spring-security-core:1.2.7.3'
-		compile ':spring-security-ldap:1.0.5'
-		compile ':tomcat:2.0.3'
+		compile	':spring-security-core:1.2.7.3'
+		compile	':spring-security-ldap:1.0.5'
+		compile	':hibernate:2.0.3'
+		runtime	':tomcat:2.0.3'
+		build ':release:2.0.0'
 		build(':svn:1.0.2') {
 			export = false
 		}
