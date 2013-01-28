a4b7bdcf27defc10c5948a6daf22197bc2b0287c
Kevin Rosso <kmr75@georgetown.edu>
2013-01-28 14:24:17 -0500

    attr value service mod

--- grails-app/services/AttributeValueService.groovy
+++ grails-app/services/AttributeValueService.groovy
@@ -4,14 +4,14 @@ class AttributeValueService {
 	
 	def findUpperRange(typeId,study){
 		def jdbcTemplate = new JdbcTemplate(dataSource) 
-		def upper = jdbcTemplate.queryForList("select max(to_number(a.value,'9999999.99')) from $study"+".Subject_Attribute_Value a where a.Attribute_TYPE_ID = " + typeId) //AttributeValue.executeQuery("select max(to_number(a.value)) from AttributeValue a where a.type.id = ?",[typeId])
+		def upper = jdbcTemplate.queryForList("select max(to_number(a.value)) from $study"+".Subject_Attribute_Value a where a.Attribute_TYPE_ID = " + typeId) //AttributeValue.executeQuery("select max(to_number(a.value)) from AttributeValue a where a.type.id = ?",[typeId])
 		def t = upper[0].values() as List
 		return new Double(t[0])
 	}
 	
 	def findLowerRange(typeId,study){
 		def jdbcTemplate = new JdbcTemplate(dataSource) 
-		def lower = jdbcTemplate.queryForList("select min(to_number(a.value,'9999999.99')) from $study"+".Subject_Attribute_Value a where a.Attribute_TYPE_ID = " + typeId)
+		def lower = jdbcTemplate.queryForList("select min(to_number(a.value)) from $study"+".Subject_Attribute_Value a where a.Attribute_TYPE_ID = " + typeId)
 		def t = lower[0].values() as List
 		return new Double(t[0])
 	}
