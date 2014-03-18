4223638f0deb19d97cc7ca8e6999d4a4d349fdaa
Robert M. Johnson <rjohnson0186@gmail.com>
2014-03-18 11:14:22 -0400

    Modified common js functions for workflow

--- web-app/js/workflow.js
+++ web-app/js/workflow.js
@@ -18,7 +18,8 @@ function eliminateDuplicates(arr) {
 
 function set_study(study_id) {
 
-	
+	console.log(study_id);
+	console.log("/" + app_name + "/studyDataSource/justSetStudy");
 	$.ajax({
 				url: "/" + app_name + "/studyDataSource/justSetStudy",
 				dataType: 'json',
@@ -29,7 +30,7 @@ function set_study(study_id) {
 						
 				},
 				error: function(request, status, error) {
-					console.log(error)
+					console.log('function set_study(study_id) has failed. Here is the reason: ' + error);
 				},
 				complete: function() {
 				}
@@ -37,7 +38,7 @@ function set_study(study_id) {
 }
 
 
-function load_studies(success_callback) {
+function load_precision_medicine_studies(success_callback) {
 
 		$.ajax({
 				url: "/" + app_name + "/studyDataSource/findStudiesForPersonalizedMedicine",
@@ -53,20 +54,53 @@ function load_studies(success_callback) {
 		});				
 }
 
+function load_population_genetics_studies(success_callback) {
 
-function get_html_for_study(study_name, study_long_name) {
-		var html = '<div id="personalized" class="gradButton gray workflowBox study">';
-		html 	+= '<div class="center-content">';
-		html 	+= '<h5>' + study_name + '</h5>';
-		html 	+= '</div>';
-		html 	+= '<p style="font-size:.8em;padding:0px">' + study_long_name + '</p>';
+		$.ajax({
+				url: "/" + app_name + "/studyDataSource/findStudiesForPopulationGenetics",
+				dataType: 'json',
+				success: function(data) {
+					success_callback(data);			
+				},
+				error: function(request, status, error) {
+					console.log(error)
+				},
+				complete: function() {
+				}
+		});				
+}
+
+function load_translational_research_studies(success_callback) {
+
+		$.ajax({
+				url: "/" + app_name + "/studyDataSource/findStudiesForTranslationalResearch",
+				dataType: 'json',
+				success: function(data) {
+					success_callback(data);			
+				},
+				error: function(request, status, error) {
+					console.log(error)
+				},
+				complete: function() {
+				}
+		});				
+}
+
+
+
+function get_html_for_study(study) {
+
+		var html = '<div id="personalized" class="gradButton gray study box">';
+		html 	+= '<h5>' + study.studyName + '</h5>';
+		html 	+= '<p class="_title"><b>Title: </b>' + study.studyLongName + '</p>';
+		html 	+= '<p class="abstract"><b>Abstract: </b>' + study.abstract + '</p>';
 		html 	+= '</div>';
 		return html;
 }
 
 
 function get_html_for_subject_type(subject_type_name) {
-		var html = '<div id="personalized" class="gradButton gray workflowBox subject_type">';
+		var html = '<div id="personalized" class="gradButton gray subject_type box">';
 		html += '<div class="center-content">';
 		html += '<h5>' + subject_type_name + '</h5>';
 		html += '</div>';
@@ -74,19 +108,25 @@ function get_html_for_subject_type(subject_type_name) {
 		return html;
 }
 
-function get_html_for_tool(tool_name) {
-		var html = '<div id="personalized" class="gradButton gray workflowBox tool">';
-		html 	+= '<div class="center-content">';
-		html 	+= '<h5>' + tool_name + '</h5>';
-		html 	+= '</div>';
-		html 	+= '</div>';
+function get_html_for_tools(tools) {
+		var html = '';
+		
+		for (var i = 0; i < tools.length; i++) {
+			html += '<a style="none" href="' + tools[i].link + '"><div id="personalized" class="gradButton gray tool box">';
+			html += '<div class="center-content">';
+			html += '<h5>' + tools[i].name + '</h5>';
+			html += '</div>';
+			html += '</div></a>';	
+		}
+		
 		return html;
 }
 
-function get_html_for_data_type(data_type_name) {
-			var html = '<div id="personalized" class="gradButton gray workflowBox data_type">';
+function get_html_for_data_type(data_type_name, study_count) {
+			var html = '<div id="personalized" class="gradButton gray data_type box">';
 			html += '<div class="center-content">';
 			html += '<h5>' + data_type_name + '</h5>';
+			html += '<span class="studycount count"> <i>studies</i><b>' + study_count + '</b></span>';
 			html += '</div>';
 			html += '</div>';
 			return html;
